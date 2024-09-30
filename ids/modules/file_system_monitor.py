d926768697b1534be55743cf7e3d2a4faf5ef940
smokydoky <dimitris.kafs@gmail.com>
2024-09-30 19:32:49 -0400

    push

--- ids/modules/file_system_monitor.py
+++ ids/modules/file_system_monitor.py
@@ -19,44 +19,21 @@ class FileSystemMonitorHandler(FileSystemEventHandler):
         if not any(
             event_src_path.startswith(excluded) for excluded in self.excluded_dirs
         ):
-            if event.event_type in ["created", "deleted", "modified", "moved"]:
-                if event_src_path in self.critical_paths:
+            # Only log critical events
+            if any(
+                event_src_path.startswith(critical) for critical in self.critical_paths
+            ):
+                if event.event_type in ["created", "deleted", "modified", "moved"]:
                     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
                     current_user = getpass.getuser()
                     message = f"{timestamp} - User: {current_user} - Critical file {event.event_type}: {event_src_path}"
                     logging.warning(message)
                     for alert in self.alerts:
                         alert.send_alert(f"Critical File {event.event_type}", message)
-                else:
-                    logging.info(
-                        f"Non-critical file {event.event_type}: {event_src_path}"
-                    )
+            else:
+                # Reduce logging for non-critical events
+                pass  # No action needed to stop logging non-critical events
         else:
-            logging.info(f"Event on excluded path: {event.src_path}")
-
-
-class FileSystemMonitor:
-    def __init__(self, alerts):
-        self.alerts = alerts
-        self.critical_paths = ["/etc/passwd", "/etc/shadow", "/etc/hosts", "/etc/group"]
-        self.watch_directories = ["/etc", "/var", "/home", "/tmp"]
-        self.excluded_dirs = ["/var/log"]
-
-    def start(self):
-        logging.info(
-            f"Starting FileSystemMonitor for directories: {self.watch_directories}"
-        )
-        event_handler = FileSystemMonitorHandler(
-            self.critical_paths, self.excluded_dirs, self.alerts
-        )
-        observer = Observer()
-        for directory in self.watch_directories:
-            observer.schedule(event_handler, path=directory, recursive=True)
-        observer.start()
-        try:
-            while True:
-                time.sleep(1)
-        except Exception as e:
-            logging.error(f"Error in FileSystemMonitor: {e}")
-            observer.stop()
-        observer.join()
+            logging.debug(
+                f"Event on excluded path: {event.src_path}"
+            )  # Changed to DEBUG
