adcca845e51a401ddc49032874fd430cd06f3895
smokydoky <dimitris.kafs@gmail.com>
2024-09-30 21:13:18 -0400

    pray

--- ids/modules/container_escape_monitor.py
+++ ids/modules/container_escape_monitor.py
@@ -11,12 +11,7 @@ class ContainerEscapeMonitor:
         self.sensitive_paths = [
             "/host_root",
             "/proc/host",
-<<<<<<< HEAD
         ]
-=======
-        ]  # Add more sensitive paths as needed
-        self.monitor_interval = 5  # seconds
->>>>>>> 154415a767f2f928ab03a07112e280b674186eb0
         logging.debug("ContainerEscapeMonitor initialized.")
 
     def start(self):
@@ -33,11 +28,7 @@ class ContainerEscapeMonitor:
         for path in self.sensitive_paths:
             if os.path.exists(path):
                 timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-<<<<<<< HEAD
                 message = f"{timestamp} - Accessed {path}. Potential container escape attempt."
-=======
-                message = f"{timestamp} - Potential container escape attempt detected: Accessed {path}"
->>>>>>> 154415a767f2f928ab03a07112e280b674186eb0
                 logging.warning(message)
                 for alert in self.alerts:
                     alert.send_alert("Container Escape Attempt Detected", message)
