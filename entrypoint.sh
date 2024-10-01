102b06d52c775899981af92738445cedd25298f2
smokydoky <dimitris.kafs@gmail.com>
2024-10-01 17:43:56 -0400

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,12 @@
 set -e
 
 # Ensure the log directories exist
-# Create directory for the log file if it doesn't exist
-# Adjust ownership and permissions
 if [ -n "$LOG_FILE_PATH" ]; then
     mkdir -p $(dirname $LOG_FILE_PATH)
-    chown -R ids_user:ids_group $(dirname $LOG_FILE_PATH)
+    # Removed chown to prevent ownership conflicts
 fi
 
-chown -R root:root /var/log/supervisor
+chown -R root:ids_group /var/log/supervisor
 
 # Set the password for ids_user if provided
 if [ -n "$IDS_USER_PASSWORD" ]; then
